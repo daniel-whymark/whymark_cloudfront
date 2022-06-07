0f3077976ac965e524bfe85ad6252b6aebb19bb3
Daniel Whymark <13569578+daniel-whymark@users.noreply.github.com>
2022-06-07 10:57:06 +0100

    Adding CF forwards

--- CF Functions/whymark-redirects.js
+++ CF Functions/whymark-redirects.js
@@ -1,40 +1,66 @@
 function handler(event) {
 
-    var request = event.request;
-    var host = event.request.headers.host.value;
-    var uri = event.request.uri;
+	var request = event.request;
+	var host = event.request.headers.host.value;
+	var uri = event.request.uri;
 
 	var code301 = false;
+	var code302 = false;
 
-    if (host.startsWith("www.") || host.startsWith("daniel.")) {
 
-        host = host.replace('www.whymark.', 'whymark.');
-        host = host.replace('daniel.whymark.', 'whymark.');
+	if (host.startsWith("www.") || host.startsWith("daniel.")) {
 
-		code301 = true;       
-    }
+		host = host.replace('www.whymark.', 'whymark.');
+		host = host.replace('www.whymark-cdn.', 'whymark-cdn.');
+		host = host.replace('daniel.whymark.', 'whymark.');
+		host = host.replace('daniel.whymark-cdn.', 'whymark-cdn.');
 
-    if (uri == "/about.html") {
+		code301 = true;
+	}
 
-		uri = uri.replace('/about.html','/');
 
-		code301 = true;       
-    }
+	if (uri == "/about.html") {
+
+		uri = uri.replace('/about.html', '/');
+
+		code301 = true;
+	}
+
+	if (host.startsWith("plex.") || host.startsWith("transmission.") || host.startsWith("tautulli.") || host.startsWith("sonarr.") || host.startsWith("nzbhydra.") || host.startsWith("radarr.") || host.startsWith("nzbget.") || host.startsWith("bazarr.") || host.startsWith("healthchecks.") || host.startsWith("unifi.") || host.startsWith("request.")) {
+
+		host = host.replace('.whymark.net', '.whymark-cdn.net');
+
+		code302 = true;
+	}
 
 	if (code301 === true) {
 
 		var response = {
-            statusCode: 301,
-            statusDescription: "Moved Permanently",
-            headers: {
-                location: {
-                    value: `https://${host}${uri}`
-                }
-            }
-        };
+			statusCode: 301,
+			statusDescription: "Moved Permanently",
+			headers: {
+				location: {
+					value: `https://${host}${uri}`
+				}
+			}
+		};
 
-        return response;
+		return response;
+
+	} else if (code302 === true) {
+
+		var response = {
+			statusCode: 302,
+			statusDescription: "Found",
+			headers: {
+				location: {
+					value: `https://${host}${uri}`
+				}
+			}
+		};
+
+		return response;
 	}
 
-    return request;
+	return request;
 }
