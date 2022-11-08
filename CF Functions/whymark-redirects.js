6f7f5f66b13f4dfb77044887ab0259662d15748c
Daniel Whymark <13569578+daniel-whymark@users.noreply.github.com>
2022-11-08 16:45:02 +0000

    Reverting last

--- CF Functions/whymark-redirects.js
+++ CF Functions/whymark-redirects.js
@@ -41,7 +41,7 @@ function handler(event) {
 		code302 = true;
 	}
 
-	if (host.startsWith("plex.") || host.startsWith("transmission.") || host.startsWith("tautulli.") || host.startsWith("sonarr.") || host.startsWith("nzbhydra.") || host.startsWith("radarr.") || host.startsWith("nzbget.") || host.startsWith("bazarr.") || host.startsWith("unifi.") || host.startsWith("request.") || host.startsWith("mastodon.")) {
+	if (host.startsWith("plex.") || host.startsWith("transmission.") || host.startsWith("tautulli.") || host.startsWith("sonarr.") || host.startsWith("nzbhydra.") || host.startsWith("radarr.") || host.startsWith("nzbget.") || host.startsWith("bazarr.") || host.startsWith("unifi.") || host.startsWith("request.")) {
 
 		host = host.replace('.whymark.net', '.whymark-cdn.net');
 
